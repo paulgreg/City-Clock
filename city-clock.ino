d209fc840150eac446a841a2a333abd033c5ebcc
Grégory Paul <paulgreg@gmail.com>
2020-01-08 23:03:39 +0100

    Try charlieplexing with 4 LEDs

--- city-clock.ino
+++ city-clock.ino
@@ -3,16 +3,24 @@
 
 #include <NTPClient.h> // https://github.com/arduino-libraries/NTPClient
 #include <WiFiUdp.h>
+#include <ChuckPlex.h> // https://github.com/marcuserronius/ChuckPlex
 
 #define DEBUG true
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
+int pins[] = {5, 4, 15}; // the pins you'll be using
+int pinCount = 3;
+int nodes = 4; // the number of LEDs
+ChuckPlex plex = ChuckPlex(pins,pinCount);
+
 void setup() {
   Serial.begin(74880);
   while (!Serial);
   Serial.println("City Clock");
+
+  if (DEBUG) plex.displayConnections(nodes);
   
   if (connectToWifi()) {
     timeClient.begin();
@@ -23,6 +31,16 @@ void loop() {
   timeClient.update();
   if (DEBUG) Serial.println(timeClient.getFormattedTime());
 
+  int led = timeClient.getSeconds() % 4;
+
+  if (DEBUG) {
+    Serial.print("enabling ");
+    Serial.println(led);
+  }
+
+  plex.clear();
+  plex.enable(led + 1);
+  
   delay(1000);
 }
 
