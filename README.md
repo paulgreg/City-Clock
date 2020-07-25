461f7029f562c36b4ef74771341f31b372500d15
Grégory Paul <paulgreg@gmail.com>
2020-07-26 00:12:28 +0200

    Working version using a PCA9685...
    To fix problem with LED too dim (ESP8266 can’t powering them)

--- README.md
+++ README.md
@@ -4,17 +4,22 @@ That project is a « fork » of the [city clock](https://www.instructables.com/i
 
 That project uses an ESP8266 instead of an arduino.
 The esp8266 connects to wifi and synchronise time via NTP at boot.
+I’m also using a PCA9685 to control LEDs (I tried Charlieplexing but LEDs were too dim, the ESP8266 isn’t powerfull enought to light them)
 
-It uses [NTPClient](https://github.com/arduino-libraries/NTPClient) for NTP and
-[ChuckPlex](https://github.com/marcuserronius/ChuckPlex) for Charlieplexing  (to connect more led than GPIO available).
+It uses [NTPClient](https://github.com/arduino-libraries/NTPClient) for NTP and [Adafruit PWM Servo driver library](https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library/).
 
 A nice [page about GPIOs to use](https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/).
 
 ## Connections
 
-- VCC to 3.3 V
-- Ground to ground
-- CH_PD from ESP to 3.3 V
+- ESP8266 VCC 3.3 V
+- ESP8266 ground to ground
+- ESP8266 CH_PD to 3.3 V
+- ESP8266 GPIO4 on PCA9685 SDA
+- ESP8266 GPIO5 on PCA9685 SDL
+- PCA9685 VCC to 3.3 V
+- PCA9685 ground to ground
+- PCA9685 power connector to ground and VCC (to power up led)
 
 And, to flash ESP8266 : 
 
@@ -22,14 +27,3 @@ And, to flash ESP8266 :
 - TX from FTDI to RX on ESP
 - DTR from FTDI to GPIO 0 on ESP
 - RST from FTDI to RESET on ESP
-
-## Connections to LED
-
-When using GPIO 5, 4, 14, 12 & 13 :
-```
-Pin 5:    1+   2+   3+   4+   5-   9-  13-
-Pin 4:    5+   6+   7+   8+   1-  10-
-Pin 14:   9+  10+  11+  12+   2-   6-
-Pin 12:  13+   3-   7-  11-
-Pin 13:   4-   8-  12-
-```
--- city-clock.ino
+++ city-clock.ino
@@ -3,102 +3,96 @@
 
 #include <NTPClient.h> // https://github.com/arduino-libraries/NTPClient
 #include <WiFiUdp.h>
-#include <ChuckPlex.h> // https://github.com/marcuserronius/ChuckPlex
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", (TIMEZONE + SUMMERTIME) * 3600, 60000);
 
-int pins[] = { 5, 4, 14, 12, 13 }; // the pins you'll be using
-int pinCount = 5;
 int LEDs = 13; // the number of LEDs
-
-ChuckPlex plex = ChuckPlex(pins,pinCount);
 
 int hours1, hours2;
 int minutes1, minutes2;
 
-unsigned int c = 0;
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(); // Default address 0x40
 
-#if DEBUG
-  #define NB_LOOP 15000
-#else     
-  #define NB_LOOP 20000
-#endif    
+void testEachLEDs() {
+  for (int l = 0; l < LEDs; l++) {
+    Serial.print("Testing LED ");
+    Serial.println(l);
+    turnLedOn(l);
+  }
+}
 
 void setup() {
   Serial.begin(115200);
   while (!Serial);
   Serial.println("City Clock");
 
-  if (DEBUG) plex.displayConnections(LEDs);
+  pwm.begin();
+  pwm.setPWMFreq(1600);
 
-  for (int i = 1; i <= LEDs; i++) {
-    Serial.print("Testing LED # ");
-    Serial.println(i);
-    plex.enable(i);
-    delay(500);
-  }
-  plex.clear();
+  while (TEST_LED) testEachLEDs();
 
   delay(500);
   if (connectToWifi()) {
     timeClient.begin();
   }
-  plex.clear();
   Serial.println("End setup");
 }
 
 void loop() {
-  if (c++ % NB_LOOP == 0) {
-
-    if (WiFi.status() != WL_CONNECTED) {
-      Serial.println("Wifi lost");
-      connectToWifi();
-      timeClient.begin();
-    }
-
-    timeClient.update();
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wifi lost");
+    connectToWifi();
+    timeClient.begin();
+  }
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());
   
-    int hours = timeClient.getHours();
-    hours1 = hours / 10;
-    hours2 = hours % 10;
-    
-    int minutes = timeClient.getMinutes();
-    minutes1 = minutes / 10;
-    minutes2 = minutes % 10;
+  int hours = timeClient.getHours();
+  hours1 = hours / 10;
+  hours2 = hours % 10;
   
-    if (DEBUG) {
-      Serial.print(hours);
-      Serial.print(":");
-      Serial.print(minutes);
-      Serial.print(":");
-      Serial.println(timeClient.getSeconds());
-    }
-  } 
-  turnAllLedsOn();
+  int minutes = timeClient.getMinutes();
+  minutes1 = minutes / 10;
+  minutes2 = minutes % 10;
+
+  handleAllLEDs();
+  
+  int waitUntilNextMinute = (60 - timeClient.getSeconds()) * 1000;
+  delay(waitUntilNextMinute);
 }
 
-inline void turnAllLedsOn() {
-  turnLedOn(1, hours1 & 2);
-  turnLedOn(2, hours1 & 1);
+inline void handleAllLEDs() {
+  handleLED(0, hours1 & 2);
+  handleLED(1, hours1 & 1);
 
-  turnLedOn(3, hours2 & 8);
-  turnLedOn(4, hours2 & 4);
-  turnLedOn(5, hours2 & 2);
-  turnLedOn(6, hours2 & 1);
+  handleLED(2, hours2 & 8);
+  handleLED(3, hours2 & 4);
+  handleLED(4, hours2 & 2);
+  handleLED(5, hours2 & 1);
 
-  turnLedOn(7, minutes1 & 4);
-  turnLedOn(8, minutes1 & 2);
-  turnLedOn(9, minutes1 & 1);
+  handleLED(6, minutes1 & 4);
+  handleLED(7, minutes1 & 2);
+  handleLED(8, minutes1 & 1);
 
-  turnLedOn(10, minutes2 & 8);
-  turnLedOn(11, minutes2 & 4);
-  turnLedOn(12, minutes2 & 2);
-  turnLedOn(13, minutes2 & 1);
+  handleLED( 9, minutes2 & 8);
+  handleLED(10, minutes2 & 4);
+  handleLED(11, minutes2 & 2);
+  handleLED(12, minutes2 & 1);
 }
 
-inline void turnLedOn(int i, boolean test) {
-  if (test) plex.enable(i);
+inline void handleLED(int i, boolean test) {
+  int maxValue = 4096;
+  pwm.setPWM(i, test ? maxValue : 0, test ? 0 : maxValue);
+}
+
+inline void turnLedOn(int l) {
+  handleLED(l, true);
+  delay(1000);
+  handleLED(l, false);
 }
 
 boolean connectToWifi() {
@@ -111,9 +105,8 @@ boolean connectToWifi() {
   unsigned int retries = 0;
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
-    plex.clear();
-    plex.enable((retries++ % LEDs) + 1);
-    delay(1000);
+    int l = retries++ % LEDs;
+    turnLedOn(l);
   }
   Serial.println("");
   Serial.println("wifi connected");
